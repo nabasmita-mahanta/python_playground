330bceb2f09f9472e47e1d6d635d1a99fc05d346
Nabasmita Mahanta <144039339+nabasmita-mahanta@users.noreply.github.com>
2023-11-18 10:31:39 +0530

    Python reloaded assignment in progress

---
+++ cloudy_ml/assigments/assignment_python_reloaded.py
@@ -0,0 +1,12 @@
+ur_age = 21
+ur_weight = 50.6
+ur_first_name = 'Mukesh'
+ur_last_name = "Manral"
+print(ur_age)
+print(ur_weight)
+print(ur_first_name)
+print(ur_last_name)
+
+print(ur_first_name == ur_last_name)
+
+

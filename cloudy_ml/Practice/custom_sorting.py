96cd9d99f16da62a9f367ba6ab16cdb6799f880f
Nabasmita Mahanta <144039339+nabasmita-mahanta@users.noreply.github.com>
2023-11-21 11:28:50 +0530

    File handling project in progress

--- cloudy_ml/Practice/custom_sorting.py
+++ cloudy_ml/Practice/custom_sorting.py
@@ -20,3 +20,24 @@ for element in my_list:
         highest_name = names
 print(highest_name)
 print(highest_marks)
+
+
+# Find out average marks scored in subject SQL?
+# Extracted col= name with SQL_data=marks
+# Extracted only SQL_data=marks
+# mathematical operation
+student_marks = [['Name',['Statistics','Python','SQL','ML','DeepLearning']],
+                 ['Ananya',[41, 34, 45, 55, 63]],
+                 ['Akash',[42, 23, 34, 44, 53]],
+                 ['Rahul',[32, 23, 13, 54, 67]],
+                 ['Gyan',[23, 82, 23, 63, 34]],
+                 ['Pranav',[21, 23, 25, 56, 56]] ]
+total_sql_marks = 0
+total_student = 0
+for element in student_marks[1:]:
+    sql_marks = element[1][2]
+    total_sql_marks = total_sql_marks + int(sql_marks)
+    total_student = total_student + 1
+
+average_marks_in_sql = total_sql_marks/ total_student
+print(average_marks_in_sql)
